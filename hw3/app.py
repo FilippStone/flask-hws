78a7742c30dece76a942e59fe529e4ea702b0859
FilippStone <95769158+FilippStone@users.noreply.github.com>
2023-08-03 15:49:26 +0300

    Create app.py

---
+++ hw3/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Подтвердите пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Зарегистрироваться')
+
+@app.route('/', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        existing_user = User.query.filter_by(username=form.username.data).first()
+        if existing_user:
+            flash('Имя пользователя уже занято', 'error')
+            return redirect(url_for('register'))
+
+        existing_email = User.query.filter_by(email=form.email.data).first()
+        if existing_email:
+            flash('Этот email уже используется', 'error')
+            return redirect(url_for('register'))
+
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Вы успешно зарегистрировались!', 'success')
+        return redirect(url_for('register'))
+
+    return render_template('register.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
