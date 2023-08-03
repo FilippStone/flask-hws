dbe4c22c4c1f0f607e7274a16d385482820d3f6f
FilippStone <95769158+FilippStone@users.noreply.github.com>
2023-08-03 16:04:58 +0300

    Update app.py

--- hw3/app.py
+++ hw3/app.py
@@ -1,48 +1,34 @@
-from flask import Flask, render_template, flash, redirect, url_for
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
 
-class RegistrationForm(FlaskForm):
-    username = StringField('Имя пользователя', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Пароль', validators=[DataRequired(), Length(min=6)])
-    confirm_password = PasswordField('Подтвердите пароль', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Зарегистрироваться')
+@app.route('/')
+def home():
+    return render_template('register.html')
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/register', methods=['POST'])
 def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        existing_user = User.query.filter_by(username=form.username.data).first()
-        if existing_user:
-            flash('Имя пользователя уже занято', 'error')
-            return redirect(url_for('register'))
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    password = generate_password_hash(request.form['password'])
 
-        existing_email = User.query.filter_by(email=form.email.data).first()
-        if existing_email:
-            flash('Этот email уже используется', 'error')
-            return redirect(url_for('register'))
+    new_user = User(first_name=first_name, last_name=last_name, email=email, password=password)
+    db.session.add(new_user)
+    db.session.commit()
 
-        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
-        db.session.add(user)
-        db.session.commit()
-
-        flash('Вы успешно зарегистрировались!', 'success')
-        return redirect(url_for('register'))
-
-    return render_template('register.html', form=form)
+    return 'Пользователь успешно зарегистрирован!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
