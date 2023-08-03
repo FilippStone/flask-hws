367e7347ca1fa9e7975238e9c7fc3c8d42991095
FilippStone <95769158+FilippStone@users.noreply.github.com>
2023-08-03 16:06:22 +0300

    Update app.py

--- hw3/app.py
+++ hw3/app.py
@@ -1,3 +1,7 @@
+# Создать форму для регистрации пользователей на сайте. 
+# Форма должна содержать поля "Имя", "Фамилия", "Email", "Пароль" и кнопку "Зарегистрироваться". 
+# При отправке формы данные должны сохраняться в базе данных, а пароль должен быть зашифрован.
+
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash
